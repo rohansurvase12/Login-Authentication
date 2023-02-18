92dffeae15bcaeb57dcabbbe4b19b596a5d9e183
Rohan Survase <rohansurvase10@gmail.com>
2023-02-18 17:47:54 +0530

    Authentication added

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBuImIMClJR2A_BTA1TbC1VJUBmXmO_AIk",
+    authDomain: "authentication-b5e8d.firebaseapp.com",
+    projectId: "authentication-b5e8d",
+    storageBucket: "authentication-b5e8d.appspot.com",
+    messagingSenderId: "638929496312",
+    appId: "1:638929496312:web:0f12f3674419e824b6330d"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
